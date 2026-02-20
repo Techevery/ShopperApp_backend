044178a9811e41b98bb0be70acaeda322db4c228
fevico <ajayifavour81@gmail.com>
2026-02-20 16:21:31 +0100

    create shopper account

--- src/shopper/shopper.controller.ts
+++ src/shopper/shopper.controller.ts
@@ -1,16 +1,13 @@
 import { Controller, Post, Body, Req, Get } from '@nestjs/common';
 import { ShopperService } from './shopper.service';
+import { CreateShopper } from './dto/shopper';
 
 @Controller('shopper')
 export class ShopperController {
     constructor(private shopperService: ShopperService){}
 
     @Post('create')
-    async createShopper(@Body() body: any){
-        try {
-            
-        } catch (error) {
-            
-        } 
+    async createShopper(@Body() body: CreateShopper){
+        return this.shopperService.createShopper(body)
     }
 } 
--- src/shopper/shopper.service.ts
+++ src/shopper/shopper.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/prisma/prisma.service';
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
+import { CreateShopper } from './dto/shopper';
 
 @Injectable()
-export class ShopperService {}
+export class ShopperService {
+  constructor(private prisma: PrismaService) {}
+
+  async createShopper(body: CreateShopper) {
+    const { name, email, regionId } = body;
+    try {
+      const shopper = await this.prisma.shopper.findUnique({
+        where: { email },
+      });
+      if (shopper) throw new UnauthorizedException('Accoun already exist!');
+      const data = await this.prisma.shopper.create({
+        data: { name, email, regionId },
+      });
+      return data
+    } catch (error) {
+        throw new HttpException(`${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  async getShoppers(){
+    try {
+        return await this.prisma.shopper.findMany()
+    } catch (error) {
+        throw new HttpException(`${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+}
