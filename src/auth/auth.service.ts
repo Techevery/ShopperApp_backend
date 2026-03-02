655235372ac4296faf4a261835fa624a0fe5fc40
fevico <ajayifavour81@gmail.com>
2026-03-02 20:16:52 +0100

    user details on login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,9 +17,10 @@ export class AuthService {
     if (!user) throw new UnauthorizedException('Invalid credentials!');
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
-    const payload = { sub: user.id, email: user.email, role: user.role };
+    const payload = { id: user.id, email: user.email, role: user.role };
     return {
       access_token: await this.jwtService.signAsync(payload),
+      payload
     };
   }
 }
