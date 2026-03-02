6dc13152b1bba681ee2a8e8817983db3becb25aa
fevico <ajayifavour81@gmail.com>
2026-03-02 11:08:02 +0100

    fixed cors issue

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+      cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true    
+    }
+  });
   app.useGlobalPipes(new ValidationPipe());
 
     const config = new DocumentBuilder()
